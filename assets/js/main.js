d8a9fd8b3c9d2a2583476a753dc14723737fdbfd
TaladropoliS <fernando.taladriz@gmail.com>
2022-04-07 13:16:24 -0400

    hode show color

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,42 @@
+// JAVASCRIPT
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+
+// JQUERY
+
 $(document).ready(function () {
-    $('.showPointer').hide();
-    $('.hideClick').click(function () {
+    $('.showPointer').dblclick();
+    $('.hideClick').dblclick(function () {
         $('.hideCard').hide();
         $('.showPointer').show();
     })
 })
 
 $(document).ready(function () {
-    $('.showCards').click(function () {
+    $('.showCards').dblclick(function () {
         $('.hideCard').show();
         $('.showPointer').hide();
+    })
+})
+
+$(document).ready(function () {
+    $('#enviarCorreo').click(function () {
+        alert('El coreo fue enviado correctamente...');
+    })
+})
+
+$(document).ready(function () {
+    $('#ingredientes').dblclick(function () {
+        $(this).css({'color': '#e70c0c'});
+    })
+})
+
+$(document).ready(function () {
+    $('#preparacion').dblclick(function () {
+        $(this).css({'color': '#e70c0c'});
     })
 })
